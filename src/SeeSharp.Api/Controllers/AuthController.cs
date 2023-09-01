647d17a20c17ad83e6bafcfc29ba5f8743349165
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2023-09-01 16:21:04 +0300

    updated controller usings

--- src/SeeSharp.Api/Controllers/AuthController.cs
+++ src/SeeSharp.Api/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SeeSharp.Application.Features.Authentication.Commands;
+using SeeSharp.Application.Features.Authentication.Commands.AuthUserCommand;
+using SeeSharp.Application.Features.Authentication.Commands.CreateUserCommand;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
