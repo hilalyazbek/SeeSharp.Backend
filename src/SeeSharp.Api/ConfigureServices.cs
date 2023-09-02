1d7937ad0f3c535da7ba25fd6997f92220052a20
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2023-09-02 18:59:01 +0300

    added administrator authorization on Post,Put,Del

--- src/SeeSharp.Api/ConfigureServices.cs
+++ src/SeeSharp.Api/ConfigureServices.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
+using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -22,34 +24,58 @@ public static class DependencyInjection
                         .AllowAnyMethod();
                 });
         });
-
-        // Add Jwt Token Options
-        var jwtSettings = configuration.GetSection("JwtOptions");
-        services.AddAuthentication(opt =>
-        {
-            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = jwtSettings["Issuer"],
-                ValidAudience = jwtSettings["Audience"],
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                    .GetBytes(jwtSettings.GetSection("Secret").Value!))
-            };
-        });
+        AddAuthenticationAndAuthorization(services, configuration);
+        services.AddAuthentication();
 
         services.AddHttpContextAccessor();
+
         services.AddScoped<ITokenGenerator, TokenGenerator>();
         services.AddScoped<SignInManager<ApplicationUser>>();
 
         return services;
+    }
+
+    private static void AddAuthenticationAndAuthorization(IServiceCollection services, IConfiguration configuration)
+    {
+        // Add Jwt Token Options
+        var jwtSettings = Guard.Against.Null(configuration.GetSection("JwtOptions"));
+
+        var secret = Guard.Against.NullOrEmpty(jwtSettings.GetValue<string>("Secret"));
+        var issuer = Guard.Against.NullOrEmpty(jwtSettings.GetValue<string>("Issuer"));
+        var audience = Guard.Against.NullOrEmpty(jwtSettings.GetValue<string>("Audience"));
+
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidIssuer = issuer,
+                ValidateAudience = true,
+                ValidAudience = audience
+            };
+        });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("Default", new AuthorizationPolicyBuilder()
+                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build());
+
+            options.AddPolicy("Administrator", new AuthorizationPolicyBuilder()
+                .RequireRole("Administrator")
+                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build());
+        });
     }
 }
 
