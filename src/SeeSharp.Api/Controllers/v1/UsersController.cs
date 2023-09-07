9405441f5be9ebc0ea1c6852ff11fb3cbad55c7d
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2023-09-07 19:07:08 +0300

    Implemented UsersController, with UserIsInRole endpoint

---
+++ src/SeeSharp.Api/Controllers/v1/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SeeSharp.Application.Features.UserManagement.Queries;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SeeSharp.Api.Controllers.v1;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("/api/v{version:apiVersion}/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet("UserIsInRole")]
+    public async Task<bool> UserIsInRole([FromBody] GetUserRoleQuery query)
+    {   
+        return await _mediator.Send(query);
+    }
+}
+
--- src/SeeSharp.Infrastructure/Identity/IdentityService.cs
+++ src/SeeSharp.Infrastructure/Identity/IdentityService.cs
@@ -59,9 +59,9 @@ public class IdentityService : IIdentityService
         return (result.ToApplicationResult(), user.Id);
     }
 
-    public async Task<bool> IsInRoleAsync(string userId, string role)
+    public async Task<bool> IsInRoleAsync(string userName, string role)
     {
-        var user = _userManager.Users.SingleOrDefault(u => u.Id == userId);
+        var user = _userManager.Users.SingleOrDefault(u => u.UserName == userName);
 
         return user != null && await _userManager.IsInRoleAsync(user, role);
     }
