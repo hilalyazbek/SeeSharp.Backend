b30278ce29490b7144b0664dc623b57d2a035b0f
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2023-09-13 22:47:30 +0300

    code cleanup

--- src/SeeSharp.Application/Features/UserManagement/Commands/UpdateUserProfileCommand/UpdateUserProfileCommand.cs
+++ src/SeeSharp.Application/Features/UserManagement/Commands/UpdateUserProfileCommand/UpdateUserProfileCommand.cs
@@ -1,13 +1,6 @@
 ﻿using Ardalis.GuardClauses;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using SeeSharp.Application.Common.Interfaces;
-using SeeSharp.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeeSharp.Application.Features.UserManagement.Commands.UpdateUserProfileCommand;
 public class UpdateUserProfileCommand : IRequest
