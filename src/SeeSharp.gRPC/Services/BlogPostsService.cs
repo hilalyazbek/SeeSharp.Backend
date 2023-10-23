eb1dc84b7dc89ab45538aaab57fd7a0a52856b7e
Hilal Yazbek <hyazbek@psa.gov.qa>
2023-10-23 13:26:35 +0300

    implemented CRUD in gRPC

--- src/SeeSharp.gRPC/Services/BlogPostsService.cs
+++ src/SeeSharp.gRPC/Services/BlogPostsService.cs
@@ -1,6 +1,8 @@
 ﻿using Grpc.Core;
 using MediatR;
 using SeeSharp.Application.Features.BlogPosts.Commands.CreateBlogPost;
+using SeeSharp.Application.Features.BlogPosts.Commands.DeleteBlogPost;
+using SeeSharp.Application.Features.BlogPosts.Commands.UpdateBlogPost;
 using SeeSharp.Application.Features.BlogPosts.Queries;
 using SeeSharp.gRPC.Protos;
 
@@ -29,6 +31,24 @@ public class BlogPostsService : BlogPosts.BlogPostsBase
         return await Task.FromResult(new CreateBlogPostResponse { Id = result.ToString() });
     }
 
+    public override async Task<GetBlogPostReponse> GetBlogPostById(GetBlogPostRequest request, ServerCallContext context)
+    {
+        var query = new GetBlogPostByIdQuery(Guid.Parse(request.Id));
+
+        var result = await _mediator.Send(query);
+
+        var response = new GetBlogPostReponse
+        {
+            Id = result.Id.ToString(),
+            Title = result.Title,
+            Category = result.Category,
+            Author = result.Author,
+            Content = result.Content,
+            DateCreated = result.DateCreated
+        };
+
+        return await Task.FromResult(response);
+    }
     public override async Task<GetBlogPostsResponse> GetBlogPosts(GetBlogPostsRequest request, ServerCallContext context)
     {
         var result = await _mediator.Send(new GetBlogPostsQuery());
@@ -47,5 +67,30 @@ public class BlogPostsService : BlogPosts.BlogPostsBase
         }));
 
         return await Task.FromResult(response);
+    }
+
+    public override async Task<empty> UpdateBlogPost(UpdateBlogPostRequest request, ServerCallContext context)
+    {
+        var command = new UpdateBlogPostCommand
+        {
+            Id = Guid.Parse(request.Id),
+            Title = request.Title,
+            Category = request.Category,
+            Author = request.Author,
+            Content = request.Content,
+        };
+
+        await _mediator.Send(command);
+
+        return new empty();
+    }
+
+    public override async Task<empty> DeleteBlogPost(DeleteBlogPostRequest request, ServerCallContext context)
+    {
+        var command = new DeleteBlogPostCommand(Guid.Parse(request.Id));
+
+        await _mediator.Send(command);
+
+        return new empty();
     }
 }
