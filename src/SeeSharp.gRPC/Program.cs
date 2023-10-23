f0fcf06d1ee09c12e8b984fc1344fe68d692afa6
Hilal Yazbek <hyazbek@psa.gov.qa>
2023-10-23 14:09:33 +0300

    Added JsonTranscoding support

--- src/SeeSharp.gRPC/Program.cs
+++ src/SeeSharp.gRPC/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
